835584f5ffacf60a3b54fbfb2a3e60cc771bfe7c
Maciek Swiech <swiechfedora@gmail.com>
2012-06-14 13:15:02 -0500

    working logging

--- iptables.sh
+++ iptables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # flush rules
-iptables -F
+iptables --flush
+iptables --delete-chain
 
 # make some more groups 
 iptables -N TCP
 iptables -N UDP
-iptables -N INBOUNDCONN
 
 # general rules
 iptables -P INPUT DROP
@@ -14,12 +14,8 @@ iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
 # allow established and lo
-iptables -A INPUT -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j INBOUNDCONN
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
-
-# log all connections
-iptables -A INBOUNDCONN -p tcp -j LOG --log-prefix ' INBOUND TCP ' --log-level 4
-iptables -A INBOUNDCONN -p tcp -j ACCEPT
 
 # dont allow invalid
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
@@ -27,7 +23,8 @@ iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 # allow pings
 iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
 
-# new not syn
+# new not syn, with logging!
+iptables -A INPUT -m conntrack --ctstate NEW -j LOG --log-level info --log-prefix '*CONN-LOG*'
 iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
 
@@ -42,6 +39,7 @@ iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
 iptables -A TCP -p tcp --dport ssh -j ACCEPT
 iptables -A TCP -p tcp --dport http -j ACCEPT
 iptables -A TCP -p tcp --dport https -j ACCEPT
+iptables -A TCP -p tcp -s 165.124.0.0/16 --dport 8080 -j ACCEPT
 
 # udp rules
 iptables -A UDP -p udp -j DROP
